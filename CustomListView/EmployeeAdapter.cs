25755623e2d3ee75a93c93228968a9b3b95ecc75
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2018-11-15 20:20:17 +0300

    Add project files.

---
+++ CustomListView/EmployeeAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+
+namespace CustomListView
+{
+    internal class EmployeeAdapter : ArrayAdapter<string>
+    {
+        private readonly string[] employeeNames;
+        private readonly Context context;
+
+        public EmployeeAdapter(Context context, int textViewResourceId, string[] objects) : base(context, textViewResourceId, objects)
+        {
+            employeeNames = objects;
+            this.context = context;
+        }
+
+        public override int Count => base.Count;
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = LayoutInflater.From(context).Inflate(Resource.Layout.list_item, null);
+
+            TextView employeeName = view.FindViewById<TextView>(Resource.Id.employee_name);
+            employeeName.Text = GetItem(position);
+
+            return view;
+        }
+    }
+}
---
+++ CustomListView/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+using v7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace CustomListView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private v7Toolbar mainToolbar;
+        private  ListView employeeList;
+        private EmployeeAdapter employeeAdapter;
+        string[] employeeNames;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            mainToolbar = FindViewById<v7Toolbar>(Resource.Id.main_toolbar);
+            SetSupportActionBar(mainToolbar);
+
+            employeeList = FindViewById<ListView>(Resource.Id.employee_listview);
+
+           employeeNames  = Resources.GetStringArray(Resource.Array.employeeArray);
+
+            employeeAdapter = new EmployeeAdapter(this, 0, employeeNames);
+            employeeList.Adapter = employeeAdapter;
+
+
+            employeeList.ItemClick += EmployeeList_ItemClick;
+        }
+
+        private void EmployeeList_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Toast.MakeText(this, employeeNames[e.Position], ToastLength.Long).Show();
+        }
+    }
+}
